c1aaadda2da571c109cae68caf58f83fe6820372
Mark Lowel Montealto <lowel.montealto@whitecloak.com>
2025-09-20 19:23:05 +0800

    Added Github Action

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    GOOGLE_API_KEY: "AIzaSyAZ-kpNiucrE3XxzJ46ZBRGMqmZWP1PXT0",
+    GOOGLE_AUTH_DOMAIN: "fir-demo-2561e.firebaseapp.com",
+    PROJECT_ID: "fir-demo-2561e",
+    storageBucket: "fir-demo-2561e.firebasestorage.app",
+    messagingSenderId: "822206099174",
+    appId: "1:822206099174:web:95a404edb6597155ce1527",    measurementId: "G-9RRK6G425W"
+};
--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,16 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { environment } from './environments/environment';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v9 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id",
-  measurementId: "G-XXXXXXXXXX"
+  apiKey: environment.GOOGLE_API_KEY.toString(),
+  authDomain: environment.GOOGLE_AUTH_DOMAIN,
+  projectId: environment.PROJECT_ID,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+  appId: environment.appId,
+  measurementId: environment.measurementId
 };
 
 // Initialize Firebase
